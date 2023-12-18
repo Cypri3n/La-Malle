e0af3842ab4b97c71cba835e607e20512de7f062
ShogunDevs <151510734+ShogunDevs@users.noreply.github.com>
2023-12-18 09:55:15 +0100

    Add files via upload

---
+++ Script_Ollivander_début.py
@@ -0,0 +1,19 @@
+def ollivander(a_rendre_gallions):
+    print("Bonjour et bienvenu chez Ollivender le vendeur de baguettes magiques ! ")
+    
+    dico_billets = {493 : 0}
+                    29 : 0
+                    1 : 0}
+    return dico_billets  
+ollivander(3)  
+    
+
+    
+  
+    
+    
+un_gallion = 493
+une_mornille = 29
+une_noise = 1
+baguette = 1151
+    
