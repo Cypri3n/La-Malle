5b255a17401ef1fe9400d93fd698a518f56bef7a
Cypri3n <114953517+Cypri3n@users.noreply.github.com>
2023-12-22 21:48:02 +0100

    ajout pygame

--- Nouveau_Main.py
+++ Nouveau_Main.py
@@ -241,4 +241,107 @@ def boutiques():
 boutiques()
 
 
+
+
+
+import pygame
+import sys
+
+
+pygame.init()
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Akhy se fait la malle")
+TEXT_COLOR = (255, 255, 255)
+
+def get_font(size):
+    return pygame.font.Font("La-Malle-main/Roboto/Roboto-Black.ttf", size)
+
+class Button():
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.original_image = image
+        self.x_pos, self.y_pos = pos
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.original_image is None:
+            self.image = self.text
+        else:
+            button_size = (200, 80)  # Set the desired button size (width, height)
+            self.image = self.resize_image(self.original_image, button_size)
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def resize_image(self, image, size):
+        # Resize the image while preserving the aspect ratio
+        aspect_ratio = image.get_width() / image.get_height()
+        new_width = int(size[1] * aspect_ratio)
+        return pygame.transform.scale(image, (new_width, size[1]))
+
+    def update(self, screen):
+        screen.blit(self.image, self.rect.topleft)
+        if self.original_image is None:
+            screen.blit(self.text, self.text_rect)
+
+    def checkForInput(self, position):
+        if self.rect.collidepoint(position):
+            return True
+        return False
+
+    def changeColor(self, position):
+        if self.rect.collidepoint(position):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
+
+def main_menu():
+    current_background = pygame.image.load("La-Malle-main/images/1302546 (1).jpg")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        SCREEN.blit(current_background, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("La-Malle-main/images/play-button_482059.png"), pos=(640, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("La-Malle-main/images/settings_3524636.png"), pos=(640, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("La-Malle-main/images/logout_1828427.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    current_background = pygame.image.load("path/to/play/background.jpg")
+                    play()
+                elif OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    current_background = pygame.image.load("path/to/options/background.jpg")
+                    options()
+                elif QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
     
