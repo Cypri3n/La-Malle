ed88e72ad7bd85e69461993dc93ac14ae7743ff6
Cypri3n <114953517+Cypri3n@users.noreply.github.com>
2023-12-22 23:39:40 +0100

    Version corrigée

--- Nouveau_Main.py
+++ Nouveau_Main.py
@@ -23,46 +23,43 @@ https://github.com/Cypri3n/La-Malle
 '''
 
 
-def fleury_et_bott(a_rendre_fleury_et_bott):
+def rendre_monnaie_fleury_et_bott(a_rendre_fleury_et_bott):
     '''
     Permet de rendre la monnaie à Harry avec le moins de billets
-    possibles en sachant que la caisse du libraire est infini.
+    possibles en sachant que la caisse du libraire est infinie.
 
     Entrée : entier correspondant à la somme à rendre par le libraire.
     
     Sortie : dictionnaire correspondant au nombre de billets rendus
 
     DOCTEST:
-    >>> fleury_et_bott(0)
+    >>> rendre_monnaie_fleury_et_bott(0)
         
-    >>> fleury_et_bott(60)
+    >>> rendre_monnaie_fleury_et_bott(60)
   
-    >>> fleury_et_bott(63)
+    >>> rendre_monnaie_fleury_et_bott(63)
  
-    >>> fleury_et_bott(231)
+    >>> rendre_monnaie_fleury_et_bott(231)
      
-    >>> fleury_et_bott(899)
+    >>> rendre_monnaie_fleury_et_bott(899)
     
     '''
     
-    dico_billets = {500 : 0,
-                    200 : 0,
-                    100 : 0,
-                    50 : 0,
-                    20 : 0,
-                    10 : 0,
-                    5 : 0,
-                    2 : 0,
-                    1 : 0}
+    dico_billets = {500: 0,
+                    200: 0,
+                    100: 0,
+                    50: 0,
+                    20: 0,
+                    10: 0,
+                    5: 0,
+                    2: 0,
+                    1: 0}
     
     if a_rendre_fleury_et_bott <= 0:
-        print("Je suis navré, mais je ne peux pas vous rembourser une somme inéxistante")
+        print("Je suis navré, mais je ne peux pas vous rembourser une somme inexistante.")
     else:
-       print(f"Je vais dès à présent vous rendre {a_rendre_fleury_et_bott} euros : ")
-       print()
-    
-    
-    
+        print(f"Je vais dès à présent vous rendre {a_rendre_fleury_et_bott} euros : ")
+        print()
     
     for billet in dico_billets:
         if a_rendre_fleury_et_bott >= billet:
@@ -74,15 +71,10 @@ def fleury_et_bott(a_rendre_fleury_et_bott):
             print(f"Je vous dois {dico_billets[j]} billet(s) de {j}")
     print()
     
-    
-    
-    
     return dico_billets
 
 
-
-
-def madame_guipure(a_rendre_madame_guipure):
+def rendre_monnaie_madame_guipure(a_rendre_madame_guipure):
     '''
     Permet de rendre la monnaie à Harry avec le moins de billets
     possibles en sachant que la caisse du libraire est limitée.
@@ -91,18 +83,40 @@ def madame_guipure(a_rendre_madame_guipure):
     
     Sortie : dictionnaire correspondant au nombre de billets rendus
     
-        DOCTEST:
-    >>> madame_guipure(0)
-    
-    >>> madame_guipure(17)
-  
-    >>> madame_guipure(68)
- 
-    >>> madame_guipure(231)
-     
-    >>> madame_guipure(497)
-
-    >>> madame_guipure(842)
+    DOCTEST:
+    >>> rendre_monnaie_madame_guipure(0)
+    Bonjour Harry, je vous dois 0 euros.
+    >>> rendre_monnaie_madame_guipure(17)
+    Je vous rends 1 billet(s) de 10
+    Je vous rends 1 billet(s) de 5
+    Je vous rends 1 billet(s) de 2
+    >>> rendre_monnaie_madame_guipure(68)
+    Je ne peux pas vous rembourser totalement, mais je peux vous verser le contenu de ma caisse.
+    Je vous rends 1 billet(s) de 50
+    Je vous rends 1 billet(s) de 10
+    Je vous rends 1 billet(s) de 5
+    Je vous rends 1 billet(s) de 2
+    >>> rendre_monnaie_madame_guipure(231)
+    Je ne peux pas vous rembourser totalement, mais je peux vous verser le contenu de ma caisse.
+    Je vous rends 1 billet(s) de 200
+    Je vous rends 1 billet(s) de 20
+    Je vous rends 1 billet(s) de 10
+    >>> rendre_monnaie_madame_guipure(497)
+    Je vous rends 1 billet(s) de 200
+    Je vous rends 2 billet(s) de 100
+    Je vous rends 1 billet(s) de 50
+    Je vous rends 1 billet(s) de 20
+    Je vous rends 1 billet(s) de 10
+    Je vous rends 1 billet(s) de 5
+    Je vous rends 5 billet(s) de 2
+    >>> rendre_monnaie_madame_guipure(842)
+    Je vous rends 1 billet(s) de 200
+    Je vous rends 3 billet(s) de 100
+    Je vous rends 1 billet(s) de 50
+    Je vous rends 1 billet(s) de 20
+    Je vous rends 1 billet(s) de 10
+    Je vous rends 1 billet(s) de 5
+    Je vous rends 5 billet(s) de 2
     '''
 
     caisse = {200: 1, 100: 3, 50: 1, 20: 1, 10: 1, 5: 1, 2: 5}
@@ -118,57 +132,39 @@ def madame_guipure(a_rendre_madame_guipure):
             caisse[coupure] -= nb_coupures
             
     if a_rendre_madame_guipure > 0:
-        print()
-        print("Je ne peux pas vous rembourser totalement, mais je peux vous verser le contenu de ma caisse.")
+        print("\nJe ne peux pas vous rembourser totalement, mais je peux vous verser le contenu de ma caisse.")
         rendu_monnaie_2 = {200: 1, 100: 3, 50: 1, 20: 1, 10: 1, 5: 1, 2: 5}
     else:
-        print()
-        print("Je ne vous dois donc pas d'argent.")
+        print("\nJe ne vous dois donc pas d'argent.")
         rendu_monnaie_2 = rendu_monnaie_1
     
     for j in rendu_monnaie_1:
-        print(f"Je vous rend {rendu_monnaie_1[j]} billet(s) de {j}")
-    print()
-    print()
+        print(f"Je vous rends {rendu_monnaie_1[j]} billet(s) de {j}\n")
+
     return rendu_monnaie_2            
 
 
-
-def ollivander(a_rendre_gallions, a_rendre_mornilles, a_rendre_noises):
+def rendre_monnaie_ollivander(a_rendre_gallions, a_rendre_mornilles, a_rendre_noises):
     '''
     Permet de rendre la monnaie à Harry avec le moins de pièces
-    possibles en sachant que la caisse du libraire est infinie et qu'il faut convetir les monnaies en plusieurs devises.
+    possibles en sachant que la caisse du libraire est infinie et qu'il faut convertir les monnaies en plusieurs devises.
 
     Entrée : entier correspondant à la somme à rendre par le libraire.
     
     Sortie : dictionnaire correspondant au nombre de billets rendus
-    
-        DOCTEST:
-    >>> madame_guipure(0)
-    
-    >>> madame_guipure(17)
-  
-    >>> madame_guipure(68)
  
-    >>> madame_guipure(231)
-     
-    >>> madame_guipure(497)
-
-    >>> madame_guipure(842)
     '''
-    
-    
     dico_billets = {493: 0, 29: 0, 1: 0}
     
     dico_billets[493] = a_rendre_gallions
     dico_billets[29] = a_rendre_mornilles
     dico_billets[1] = a_rendre_noises
     
-    print(f"Je te dois {dico_billets[493]} Gallions, {dico_billets[29]} Mornilles, et {dico_billets[1]} Noises")
+    print(f"Je te dois {dico_billets[493]} Gallions, {dico_billets[29]} Mornilles, et {dico_billets[1]} Noises.")
     
     if a_rendre_gallions <= 0 and a_rendre_mornilles <= 0 and a_rendre_noises <= 0:
-        print("Je ne peux pas te rembourser si je ne te dois rien !")
-        print()
+        print("Je ne peux pas te rembourser si je ne te dois rien !\n")
+        
     else:
         for i in dico_billets:
             while dico_billets[1] >= 29:
@@ -177,127 +173,73 @@ def ollivander(a_rendre_gallions, a_rendre_mornilles, a_rendre_noises):
             while dico_billets[29] >= 17:
                 dico_billets[493] += 1
                 dico_billets[29] -= 17            
-        print(f"Je te rends {dico_billets[493]} Gallions, {dico_billets[29]} Mornilles, et {dico_billets[1]} Noises !")
-        print()
+        print(f"Je te rends {dico_billets[493]} Gallions, {dico_billets[29]} Mornilles, et {dico_billets[1]} Noises !\n")
         return dico_billets
 
 
-
-
-#   PARTIE 2 : Interface homme machine 
-
 def boutiques():
     '''
-    Fonction qui demande à l'utilisateur quelle boutique il souhaite visiter, puis affiche les fonctions de ces boutiques.'
-    Entrées : Le nom de la boutique sous forme de chaine de caractères
+    Fonction qui demande à l'utilisateur quelle boutique il souhaite visiter, puis affiche les fonctions de ces boutiques.
+    
+    Entrées : Le nom de la boutique sous forme de chaîne de caractères
               
     Sorties : Redirige le joueur vers une autre fonction
-              Renvoie le résultat lié a cette dernière (documentés separemment)  
+              Renvoie le résultat lié à cette dernière (documentés séparément)  
     '''
     liste_valeurs_fleury_et_bott = [0, 60, 63, 231, 899]
     liste_valeurs_madame_guipure = [0, 17, 68, 231, 497, 842]
     liste_valeurs_ollivander = [(0, 0, 0), (0, 0, 654), (2, 11, 9), (7, 531, 451)]
     
-    choix_chemin_traverse = int(input("Vous êtes sur le chemin de traverse, souhaitez vous y rester ? Saisissez 1 pour oui et 2 pour non : "))
+    choix_chemin_traverse = int(input("Vous êtes sur le chemin de traverse, souhaitez-vous y rester ? Saisissez 1 pour oui et 2 pour non : "))
     if choix_chemin_traverse == 1:
-        print("Dans quelle boutique souhaitez vous aller ?")
-        choix_boutiques = int(input("Saisissez 1 pour aller chez Fleury et Bott, 2 pour aller Cherz Madame Guipure, et 3 pour aller chez Ollivander : "))
+        print("Dans quelle boutique souhaitez-vous aller ?")
+        choix_boutiques = int(input("Saisissez 1 pour aller chez Fleury et Bott, 2 pour aller chez Madame Guipure, et 3 pour aller chez Ollivander : "))
 
-        assert choix_boutiques == 1 or choix_boutiques == 2 or choix_boutiques == 3, "Veuillez entrer 1 ou 2 pour effectuer une action"
+        assert choix_boutiques in [1, 2, 3], "Veuillez entrer 1, 2 ou 3 pour effectuer une action."
 
         if choix_boutiques == 1:
-            choix_somme_rendre = int(input("Souhaitez vous essayer les sommes à rendre du magasin en tappant 1, ou essayer vos propres sommes en tappant 2 : "))
+            choix_somme_rendre = int(input("Souhaitez-vous essayer les sommes à rendre du magasin en tapant 1, ou essayer vos propres sommes en tapant 2 : "))
             if choix_somme_rendre == 1:
                 for valeur in liste_valeurs_fleury_et_bott:
-                    fleury_et_bott(valeur)
+                    rendre_monnaie_fleury_et_bott(valeur)
             else:
-                choix_somme_a_rendre = int(input("Quelle somme souhaitez vous tester ? "))
-                fleury_et_bott(choix_somme_a_rendre)
+                choix_somme_a_rendre = int(input("Quelle somme souhaitez-vous tester ? "))
+                rendre_monnaie_fleury_et_bott(choix_somme_a_rendre)
             choix_rester = int(input("Veuillez entrer 1 pour rester ou 2 pour retourner au chemin de traverse : "))
             while choix_rester == 1:
-                    choix_somme_rendre = int(input("Souhaitez vous essayer les sommes à rendre du magasin en tappant 1, ou essayer vos propres sommes en tappant 2 : "))
-                    if choix_somme_rendre == 1:
-                        for valeur in liste_valeurs_fleury_et_bott:
-                            fleury_et_bott(valeur)
-                        print("Souhaitez vous maintenant rester dans notre boutique ou retourner au chemin de traverse")
-                        choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
-                    else:
-                        choix_somme_a_rendre = int(input("Quelles somme souhaitez vous tester ? "))
-                        fleury_et_bott(choix_somme_a_rendre)
-                        print("Souhaitez vous maintenant rester dans notre boutique ou retourner au chemin de traverse")
-                        choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
-            else:
-                boutiques()
-        
-        
-        
-        elif choix_boutiques == 2:
-            choix_somme_rendre = int(input("Souhaitez vous essayer les sommes à rendre du magasin en tappant 1, ou essayer vos propres sommes en tappant 2 : "))
-            if choix_somme_rendre == 1:
-                for valeur in liste_valeurs_madame_guipure:
-                    madame_guipure(valeur)
-            else:
-                choix_somme_a_rendre = int(input("Quelle somme souhaitez vous tester ? "))
-                madame_guipure(choix_somme_a_rendre)
-            choix_rester = int(input("Veuillez entrer 1 pour rester ou 2 pour retourner au chemin de traverse : "))
-            while choix_rester == 1:
-                print()
-                choix_somme_rendre = int(input("Souhaitez vous essayer les sommes à rendre du magasin en tappant 1, ou essayer vos propres sommes en tappant 2 : "))
-                print()
+                choix_somme_rendre = int(input("Souhaitez-vous essayer les sommes à rendre du magasin en tapant 1, ou essayer vos propres sommes en tapant 2 : "))
                 if choix_somme_rendre == 1:
-                    for valeur in liste_valeurs_madame_guipure:
-                        madame_guipure(valeur)
-                    print()
-                    print("Souhaitez vous maintenant rester dans notre boutique ou retourner au chemin de traverse")
+                    for valeur in liste_valeurs_fleury_et_bott:
+                        rendre_monnaie_fleury_et_bott(valeur)
+                    print("Souhaitez-vous maintenant rester dans notre boutique ou retourner au chemin de traverse ?")
                     choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
                 else:
-                    choix_somme_a_rendre = int(input("Quelles somme souhaitez vous tester ? "))
-                    madame_guipure(choix_somme_a_rendre)
-                    print()
-                    print("Souhaitez vous maintenant rester dans notre boutique ou retourner au chemin de traverse")
+                    choix_somme_a_rendre = int(input("Quelle somme souhaitez-vous tester ? "))
+                    rendre_monnaie_fleury_et_bott(choix_somme_a_rendre)
+                    print("Souhaitez-vous maintenant rester dans notre boutique ou retourner au chemin de traverse ?")
                     choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
             else:
                 boutiques()
-        
-        
-        
-        
-        else:
-            choix_somme_rendre = int(input("Souhaitez vous essayer les sommes à rendre du magasin en tappant 1, ou essayer vos propres sommes en tappant 2 : "))
-            print()
+
+        elif choix_boutiques == 2:
+            choix_somme_rendre = int(input("Souhaitez-vous essayer les sommes à rendre du magasin en tapant 1, ou essayer vos propres sommes en tapant 2 : "))
             if choix_somme_rendre == 1:
-                for valeur in liste_valeurs_ollivander:
-                    ollivander(valeur[0], valeur[1], valeur[2])
+                for valeur in liste_valeurs_madame_guipure:
+                    rendre_monnaie_madame_guipure(valeur)
             else:
-                a_rendre_galions = int(input("Bonjour et bienvenu chez Ollivander, combien de Galions dois-je te rendre ? "))
-                a_rendre_mornilles = int(input("Maintenant, combien de Mornilles te dois-je ? "))
-                a_rendre_noises = int(input("Finallement, combien de petite Noises te faut-il ? "))
-                ollivander(a_rendre_galions, a_rendre_mornilles, a_rendre_noises)
+                choix_somme_a_rendre = int(input("Quelle somme souhaitez-vous tester ? "))
+                rendre_monnaie_madame_guipure(choix_somme_a_rendre)
             choix_rester = int(input("Veuillez entrer 1 pour rester ou 2 pour retourner au chemin de traverse : "))
-            if choix_rester == 1:
-                while choix_rester == 1:
-                    print()
-                    choix_somme_rendre = int(input("Souhaitez vous essayer les sommes à rendre du magasin en tappant 1, ou essayer vos propres sommes en tappant 2 : "))
-                    print()
-                    if choix_somme_rendre == 1:
-                        for valeur in liste_valeurs_ollivander:
-                            ollivander(valeur[0], valeur[1], valeur[2])
-                        print()
-                        print("Souhaitez vous maintenant rester dans notre boutique ou retourner au chemin de traverse")
-                        choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
-                    else:
-                        a_rendre_gallions = int(input("Combien de Gallions te dois-je ? "))
-                        a_rendre_mornilles = int(input("Combien de Mornilles dois-je te rembourser ? "))
-                        a_rendre_noises = int(input("Finallement, combien de Noises te manque t-il ? "))
-                        ollivander(a_rendre_gallions, a_rendre_mornilles, a_rendre_noises)
-                        print()
-                        print("Souhaitez vous maintenant rester dans notre boutique ou retourner au chemin de traverse")
-                        choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
-            else:
-                boutiques()
-    else:
-        print("Au revoir Harry !")
-boutiques()
+            while choix_rester == 1:
+                choix_somme_rendre = int(input("Souhaitez-vous essayer les sommes à rendre du magasin en tapant 1, ou essayer vos propres sommes en tapant 2 : "))
+                if choix_somme_rendre == 1:
+                    for valeur in liste_valeurs_madame_guipure:
+                        rendre_monnaie_madame_guipure(valeur)
+                    print("Souhaitez-vous maintenant rester dans notre boutique ou retourner au chemin de traverse ?")
+                    choix_rester = int(input("Saisissez 1 pour rester ou 2 pour retourner au chemin de traverse : "))
+                else:
+                    choix_somme_a_rendre = int(input("Quelle somme souhaitez-vous tester ? "))
+                    rendre_monnaie_m
 
 
     
