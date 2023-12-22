6695f26ec51101d374e63c0ee5315d4121df40e6
Cypri3n <114953517+Cypri3n@users.noreply.github.com>
2023-12-22 12:01:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -240,25 +240,74 @@ boutiques()
 
 import pygame
 
-pygame.init()
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
-screen = pygame.display.set_mode((SCREEN_WDITH, SCREEEN_HEIGHT))
-pygame.display.set_caption("Akhy se fait la malle")
-pygame.font.SysFont("arialblack", 40)
-TEXT.COL = (255, 255, 255)
+#Classe boutton
+class Button():
+    def __init__(self, image, pos, text_input, base_color, hovering_color):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y.pos = pos[0]
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+def update(self, screen):
+    if self.image is not None:
+        self.blit(self.image, self.rect)
+    screen.blit(self.text, self.text_rect)
 
-def draw_text(text, font, text_col, x, y):
-    img = font.render(text, Ttrue, text_col)
-    screen.blit(img, (x, y))
-run = True
-while run:
-    screen.fill((52, 78, 91))
-    draw_text("Appuyer sur ENTREE pour commencer")
+def checkForInput(self, position):
+    if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+        return True
+    return False
+def changeColor(self, poition):
+    if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+        self.text = self.font.render(self.text_input, True, self.hovering_color)
+    else:
+        self.text = self.font.render(self.text_input, True, self.base_color)
 
-    for event in pygame.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
-pygame.quit()
+def main_menu():
+    pygame.display.set_caption("Harry, le prince du shopping")
 
+    while True:
+        SCREEN.blit(BG, (0,0))
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+        MENU_TEXT = get_font(100).render("MENU PRINCIPAL"), True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640,100))
+        
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640,250),
+                             text_input="PLAY", font=get(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640,250),
+                             text_input="OPTIONS", font=get(75), base_color="#d7fcd4", hobering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640,550),
+                             text_input="QUIT", font=get(75), base_color="#d7fcd4", hobering_color="White")
+
+
+        
+# pygame.init()
+# SCREEN_WIDTH = 1200
+# SCREEN_HEIGHT = 800
+# screen = pygame.display.set_mode((SCREEN_WDITH, SCREEEN_HEIGHT))
+# pygame.display.set_caption("Akhy se fait la malle")
+# pygame.font.SysFont("arialblack", 40)
+# TEXT.COL = (255, 255, 255)
+
+# def draw_text(text, font, text_col, x, y):
+#     img = font.render(text, Ttrue, text_col)
+#     screen.blit(img, (x, y))
+# run = True
+# while run:
+#     screen.fill((52, 78, 91))
+#     draw_text("Appuyer sur ENTREE pour commencer")
+
+#     for event in pygame.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+#     pygame.display.update()
+# pygame.quit()
+
+# def jouer():
+#     while 
